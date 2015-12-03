7e038ea1d30b25703db8171a9b3ac7e44adcf96b
Kamuela Franco <kamuela.franco@gmail.com>
2015-12-03 02:39:42 +0000

    Moved to Koa from Express

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,28 +6,28 @@ const FILL_RESULTS = 'FILL_RESULTS';
 
 // Define action creators
 function search(query) {
-	return { type: SEARCH, query };
+    return {type: SEARCH, query};
 }
 
 function nextPage(steps) {
-	return { type: GO_FORWARD, steps };
+    return {type: GO_FORWARD, steps};
 }
 
 function prevPage(steps) {
-	return { type: GO_BACKWARD, steps };
+    return {type: GO_BACKWARD, steps};
 }
 
 function fillResults(results) {
-	return { type: FILL_RESULTS, results }
+    return {type: FILL_RESULTS, results}
 }
 
 module.exports = {
-	GO_FORWARD,
-	GO_BACKWARD,
-	SEARCH,
-	FILL_RESULTS,
-	nextPage,
-	prevPage,
-	search,
-	fillResults
+    GO_FORWARD,
+    GO_BACKWARD,
+    SEARCH,
+    FILL_RESULTS,
+    nextPage,
+    prevPage,
+    search,
+    fillResults
 };
